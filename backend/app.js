6c4e472361f2f36812c998ad720acbd2ff2ad6f6
Viona <VTHY17.19@ichat.sp.edu.sg>
2021-08-08 20:06:26 +0800

    remove comment

--- backend/app.js
+++ backend/app.js
@@ -5,79 +5,11 @@ const db = require('./query')
 app.use(cors()); // returns a fuction that is a middleware
 app.use(express.json()); // recognize the incoming Request Object as a JSON Object
 
-// var jokedatabase  = [
-//   {
-//      joke: 'joke1',
-//      answer: 'answer1',
-//      likes: 300
-//  },
-//   {
-//      joke: 'joke2',
-//      answer: 'answer2',
-//      likes: 300
-//  },
-//   {
-//      joke: 'joke3',
-//      answer: 'answer3',
-//      likes: 300
-//  },
-//  {
-//      joke: 'joke4',
-//      answer: 'answer4',
-//      likes: 300
-//  },
-//  {
-//      joke: 'joke5',
-//      answer: 'answer5',
-//      likes: 500
-//  },
-// ]
-
 app.post('/form', db.submit)
-// app.post('/form', function (req, res, next) {
-//   console.log('ehueh')
-//   console.log(req.body)
-//   //const {joke, answer} = req.query;
-//   // jokedatabase.push( req.body)
-//   // console.log(jokedatabase)
-//   return res.json(req.body) // recommend using res.json
-// })
-
 app.get('/jokes', db.getAllJokes)
-// app.get('/jokes', function (req, res, next) {
-//   jokedatabase.sort((a, b) => b.joke - a.joke);
-//   console.log(jokedatabase)
-//   var jokeid = req.query.jokeid
-//   console.log('appjoke:' + jokeid)
-//   if(jokeid == jokedatabase.length){
-//     console.log('end')
-//     return res.status(404).send('End of jokes, come back next time..')
-//   }else{
-//     return res.json(jokedatabase[jokeid]) // recommend using res.json
-//   }
-// })
-
 app.put('/like', db.like)
 app.put('/likeTop', db.likeTop)
-// app.put('/like', function(req,res,next) {
-//   var jokeid = req.query.jokeid
-//   console.log('id:' + jokeid)
-//   //var like = req.body.addlike
-//   jokedatabase[jokeid].likes += 1
-//   console.log(jokedatabase[jokeid].likes)
-//   return res.sendStatus(200)
-// })
-
 app.get('/scoreboard', db.getTopJokes)
-// app.get('/scoreboard', function (req, res, next) {
-//   //jokedatabase.sort((a, b) => b.likes - a.likes);
-// var sortedJokes = jokedatabase.slice().sort((a, b) => b.likes - a.likes)
-//   // use Array.map()
-//   // console.log(jokedatabase)
-//   console.log(sortedJokes)
-//   return res.json(sortedJokes)
-
-// })
 
 app.listen(8000, function () {
   console.log('listening on port 8000')
